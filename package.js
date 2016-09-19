9972b43d31fff5d50fc7e6e193a851065c48fb9f
cookwu <privateerxp@gmail.com>
2016-09-19 14:55:04 +0800

    update opentok layout and main js to new version

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "Integrate real-time communications into your website and mobile apps using the OpenTok WebRTC API.",
-  version: "2.3.0",
-  name: "aspin:tokbox",
-  git: "https://github.com/aspin/tokbox-meteor"
+  version: "2.4.0",
+  name: "cookwu:tokbox",
+  git: "https://github.com/cookwu/tokbox-meteor"
 });
 
-Npm.depends({"opentok" : "2.3.0"});
+Npm.depends({"opentok" : "2.4.0"});
 
 Package.onUse(function(api) {
-  api.versionsFrom('0.9.0');
+  api.versionsFrom('1.4.1.1');
   api.addFiles('server/index.js', 'server');
   api.addFiles(['client/opentok.min.js', 'client/opentok-layout.min.js'], 'client');
   api.export(['OpenTok', 'OpenTokClient'], 'server');
